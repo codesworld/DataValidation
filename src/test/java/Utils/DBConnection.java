ac41920af39d37c1367688e3eef956ca413b9105
Hacer <hacer.karademir@outlook.com>
2025-01-29 21:51:20 +0000

    Update env

--- src/test/java/Utils/DBConnection.java
+++ src/test/java/Utils/DBConnection.java
@@ -12,10 +12,11 @@ public class DBConnection {
             checkRequiredVars("DB_URL", "DB_USERNAME", "DB_PASSWORD", "DB_DRIVER");
             Class.forName(ConfigReader.get("DB_DRIVER"));
             connection = DriverManager.getConnection(
-                    ConfigReader.get("DB_URL"),
-                    ConfigReader.get("DB_USERNAME"),
-                    ConfigReader.get("DB_PASSWORD")
+                    System.getenv("DB_URL") != null ? System.getenv("DB_URL") : ConfigReader.get("DB_URL"),
+                    System.getenv("DB_USERNAME") != null ? System.getenv("DB_USERNAME") : ConfigReader.get("DB_USERNAME"),
+                    System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : ConfigReader.get("DB_PASSWORD")
             );
+
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException("Failed to connect to the database.", e);
         }
